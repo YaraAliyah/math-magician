ecc9d14faeccb54deea9ab29abd601f1570caa15
dtrunkl <115902953+dtrunkl@users.noreply.github.com>
2026-03-03 11:19:25 +0100

    Create addition.py

---
+++ addition.py
@@ -0,0 +1,9 @@
+def addition(a, b):
+    return a + b
+
+# Beispiel
+zahl1 = float(input("Erste Zahl: "))
+zahl2 = float(input("Zweite Zahl: "))
+
+ergebnis = addition(zahl1, zahl2)
+print(f"Ergebnis: {zahl1} + {zahl2} = {ergebnis}")
