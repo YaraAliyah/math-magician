bd55a2fd81f0de194ae983759a5858886f4fed64
chiaraaa1234 <zunchd23@htl-kaindorf.at>
2026-03-17 11:12:16 +0100

    Add simple subtraction function

--- math.py
+++ math.py
@@ -9,8 +9,14 @@ def main():
     a = float(sys.argv[2])
     b = float(sys.argv[3])
 
+    if op == "-":
+        result = subtract(a, b)
 
     print("Ende")
 
 if __name__ == "__main__":
     main()
+
+
+def subtract(a, b):
+    return a - b
